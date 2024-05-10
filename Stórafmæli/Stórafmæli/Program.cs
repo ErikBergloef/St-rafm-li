322c7859a35953ecd030f5ddea1c34517330bb22
ErikBergloef <47384098+ErikBergloef@users.noreply.github.com>
2024-05-10 08:54:45 +0200

    Add files via upload

---
+++ Stórafmæli/Stórafmæli/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Stórafmæli
+{
+    internal class Birthday
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are Jónas turning tomorrow? Specify the number: ");
+
+            int age = int.Parse(Console.ReadLine());
+
+            // Then checking if the input is divisible evenly by 10.
+
+            if (age % 10 == 0)  
+            {
+                Console.WriteLine("Jebb");
+            }
+            else
+            {
+                Console.WriteLine("Neibb");
+            }
+
+
+            }
+        }
+    }
